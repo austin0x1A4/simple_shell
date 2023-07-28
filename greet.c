07f9903bf7c920857e273151a998c3299a4fb232
austin0x1A4 <karurimm@gmail.com>
2023-07-28 06:03:48 -0700

    first commit

---
+++ greet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+int main() {
+    /* Call the greet() function to print a greeting message */
+    greet();
+    return 0;
+}
+
+/**
+ * Function to greet the user with "Hello, World!"
+ */
+void greet() {
+    /* Print the greeting message */
+    printf("Hello, World!\n");
+}
+
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Function prototype for greet()
+ */
+void greet();
+
+#endif /* MAIN_H */
+
